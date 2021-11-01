ec1eb379722c4a4749ebcb1ec085f02b050a8a64
Sang Cá Mập <55171560+sangcamap@users.noreply.github.com>
2021-11-01 22:48:02 +0700

    2/3 trang home

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$('.sale__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1600,
+    arrows: true,
+  });
+$('.list').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 1600,
+    arrows: true,
+  });
