ee91f69c912f8d28d7b231a405c0c49092366806
Sang Cá Mập <55171560+sangcamap@users.noreply.github.com>
2021-11-17 16:57:23 +0700

    thêm data fix popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,20 @@ $('.feedback__slider').slick({
 
 
 //#MỞ OVERVIEW SẢN PHẨM KHI CLICK
-
 document.querySelectorAll('.product').forEach((product) =>{
   product.addEventListener('click',  () => {
-    console.log(product.value)
+    changeProduct(product.value)
     document.getElementById('popup').style.display = "flex";
+    if (product.value < 7){
+      document.querySelector('.product-overview__container__right--size').style.display = "block"
+      document.querySelector('.product-overview__container__right--base').style.display = "block"
+      document.querySelector('.product-overview__container__right--more').style.display = "block"
+    }
+    else{
+      document.querySelector('.product-overview__container__right--size').style.display = "none"
+      document.querySelector('.product-overview__container__right--base').style.display = "none"
+      document.querySelector('.product-overview__container__right--more').style.display = "none"
+    }
   })
 })
 
@@ -37,3 +46,10 @@ document.getElementById('close-overview-btn').addEventListener('click', ()=>{
   document.getElementById('popup').style.display = "none"
 })
 
+//#THAY ĐỔI ĐỐI TƯỢNG TRONG POP-UP
+function changeProduct(index){
+  document.getElementById('product__name').innerText = product[index].name;
+  document.getElementById('product__price').innerText = product[index].price;
+  document.getElementById('product__description').innerText = product[index].description;
+  document.getElementById('product__pic').src = product[index].pic;
+}
