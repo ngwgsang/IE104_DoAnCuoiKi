fad990b6ed276a67a17bc0f374b8548e39a96925
Sang Cá Mập <55171560+sangcamap@users.noreply.github.com>
2021-11-21 14:54:05 +0700

    đổi tên class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 //#SLICK SLIDER
-$('.sale__slider').slick({
+$('.home-promotion__slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
   arrows: true,
 });
-$('.list').slick({
+$('.home-menu__list').slick({
   slidesToShow: 3,
   slidesToScroll: 2,
   autoplay: true,
   autoplaySpeed: 4000,
   arrows: true,
 });
-$('.feedback__slider').slick({
+$('.home-feedback__slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
